624923dda34a68ad12c4818db394ca23d38c15fb
Daniel Hopkinson <danhopkinson28@gmail.com>
2021-03-07 18:27:58 +0000

    Added Basic Collision Detection
    Added a basic script that detects what the rocket collided with and then prints a line to the debug console to say what happened.

---
+++ Scripts/CollisionHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour {
+    void OnCollisionEnter(Collision other) {
+        switch (other.gameObject.tag) {
+            case "Launch Pad":
+                Debug.Log("Ready to Launch");
+                break;
+            case "Landing Pad":
+                Debug.Log("Level complete");
+                break;
+            case "Fuel Pickup":
+                Debug.Log("Refuelled");
+                break;
+            default:
+                Debug.Log("You crashed");
+                break;
+        }
+    }
+}
